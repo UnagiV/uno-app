e59e91e34337d4a799d4bd35833d55a3b65d05a5
Yanng <Yanng@DESKTOP-Q1CIPV8>
2021-10-07 21:48:12 +0200

    add routing and lobby component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { LobbyComponent } from './components/lobby/lobby.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule],
+  declarations: [AppComponent, LobbyComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
